3dbf34326c0bde542f9342271ad3ac0a2f1b62e4
harshalgalgale <h.galgale@gmail.com>
2020-10-18 01:16:46 +0100

    Message admin page changes

--- src/chat/admin.py
+++ src/chat/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Contact, Chat, Message
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('contact', 'content', 'timestamp')
+
 admin.site.register(Chat)
 admin.site.register(Contact)
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
