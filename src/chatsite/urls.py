702e37666807503c770fcaef094780da59edda94
harshalgalgale <h.galgale@gmail.com>
2020-10-18 16:14:25 +0100

    URLs

--- src/chatsite/urls.py
+++ src/chatsite/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('chat/', include('chat.api.urls', namespace='chat')),
-    # path('rest-auth/', include('rest_auth.urls')),
-    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    # re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
 ]
